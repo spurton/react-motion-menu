028230db41218e8bcbf5b30d2baf9244b0eac1e6
Scott Burton <spurton@gmail.com>
2017-08-24 14:44:41 -0700

    Arc example

--- docs/src/example.js
+++ docs/src/example.js
@@ -3,24 +3,24 @@ import MotionMenu from '../../src';
 
 export default () => (
   <MotionMenu
-    type="circle"
-    margin={120}
+    type="arc"
+    margin={-120}
     y={0}
     bumpy
     x={0}
     openSpeed={60}
     wing={false}
-     reverse={false}
-     onOpen={() => console.log('onOpen')}
-     onClose={() => console.log('onClose')}
+    reverse={false}
+    onOpen={() => console.log('onOpen')}
+    onClose={() => console.log('onClose')}
   >
-    <div className="button"><i className="fa fa-bars" /></div>
-    <div className="button"><i className="fa fa-cogs" /></div>
-    <div className="button"><i className="fa fa-cloud" /></div>
-    <div className="button"><i className="fa fa-home" /></div>
-    <div className="button"><i className="fa fa-flash" /></div>
-    <div className="button"><i className="fa fa-heart" /></div>
-    <div className="button"><i className="fa fa-globe" /></div>
-    <div className="button"><i className="fa fa-plug" /></div>
+    <div className="circle"><i className="fa fa-bars" /></div>
+    <div className="circle"><i className="fa fa-cogs" /></div>
+    <div className="circle"><i className="fa fa-cloud" /></div>
+    <div className="circle"><i className="fa fa-home" /></div>
+    <div className="circle"><i className="fa fa-flash" /></div>
+    <div className="circle"><i className="fa fa-heart" /></div>
+    <div className="circle"><i className="fa fa-globe" /></div>
+    <div className="circle"><i className="fa fa-cogs" /></div>
   </MotionMenu>
 );
